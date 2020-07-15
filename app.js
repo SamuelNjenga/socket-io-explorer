d8dee8179fe04206d44c3d089d5ae020863fe674
Samuel Njenga <snjenga704@gmail.com>
2020-07-15 21:51:00 +0300

    Modified app.js and index.html to emit events as a client has submitted a form

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ io.on('connection', (socket) => {
       console.log('user disconnected'); //Each socket also fires a special disconnect event:
     });
   });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+  
 http.listen(3000, () => {
   console.log('listening on *:3000')
 })
