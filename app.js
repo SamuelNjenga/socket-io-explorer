83126dfc15500aa5a4799db6b28433ad13be8e05
Samuel Njenga <snjenga704@gmail.com>
2020-07-15 21:28:54 +0300

    Added index.html which sends a connection or disconnection msg when client connects or disconnects

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const io = require('socket.io')(http); //initialize a new instance of socket.io by passing the http (the HTTP server) object
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to the socket.io apps</h1>')
+  res.sendFile(__dirname + '/index.html')
 })
 
+//listen on the connection event for incoming sockets and log it to the console
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected'); //Each socket also fires a special disconnect event:
+    });
+  });
 http.listen(3000, () => {
   console.log('listening on *:3000')
 })
