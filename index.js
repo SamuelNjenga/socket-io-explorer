1a271e414c9bd2bc9ca0c992b05427d47f38ebb1
Samuel Njenga <snjenga704@gmail.com>
2020-07-16 15:52:59 +0300

    Updated index.html and index.js which sends events from server to the client using socket.emit

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
 
 const httpServer = require('http').createServer((req, res) => {
-  // serve the index.html file
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Length', Buffer.byteLength(content));
-  res.end(content);
+    // serve the index.html file
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(content));
+    res.end(content);
 });
 
 const io = require('socket.io')(httpServer);
 
 io.on('connect', socket => {
-  console.log('connect');
+    let counter = 0;
+    setInterval(() => {
+        socket.emit('hello', ++counter);
+    }, 1000);
 });
 
 httpServer.listen(3000, () => {
-  console.log('go to http://localhost:3000');
+    console.log('go to http://localhost:3000');
 });
