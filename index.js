147dd8c74fe132d7c0179f5909a814e105c03a31
Samuel Njenga <snjenga704@gmail.com>
2020-07-16 15:46:31 +0300

    Added index.js and index.html which uses socket.emit and socket.io

---
+++ index.js
@@ -0,0 +1,18 @@
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
+
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(content));
+  res.end(content);
+});
+
+const io = require('socket.io')(httpServer);
+
+io.on('connect', socket => {
+  console.log('connect');
+});
+
+httpServer.listen(3000, () => {
+  console.log('go to http://localhost:3000');
+});
